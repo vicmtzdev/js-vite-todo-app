d8dc5f3cf570e71258f2538571982a3c8304f4f4
Victor Martinez <vicmtzdev@gmail.com>
2023-06-07 10:51:00 -0600

    methods done

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -10,7 +10,9 @@ const state = {
 
     todos: [
         new Todo('Piedra del alma'),
+        new Todo('Piedra del poder'),
         new Todo('Piedra del tiempo'),
+        new Todo('Piedra del universo'),
         new Todo('Piedra del infinito'),
     ],
 
@@ -29,32 +31,78 @@ const loadStore = () => {
     throw new Error('Not implemented');
 }
 
+const getTodos = (filter = Filters.All) => {
+
+    switch (filter) {
+        case Filters.All:
+            return [...state.todos];
+
+        case Filters.Completed:
+            return state.todos.filter(todo => todo.done);
+
+        case Filters.Pending:
+            return state.todos.filter(todo => !todo.done);
+
+        default:
+            throw new Error('Option ${filter} is not valid');
+    }
+
+}
+
+
 /**
  * 
  * @param {String} description 
  */
 const addTodo = (description) => {
-    throw new Error('Not implemented');
+
+    if (!description) throw new Error('Description ir required');
+
+    state.todos.push(new Todo(description));
+
 }
 
 const toggleTodo = (todoId) => {
-    throw new Error('Not implemented');
+
+    state.todos = state.todos.map(todo => {
+
+        if (todo.id === todoId) {
+            todo.done = !todo.done;
+        }
+
+        return todo;
+
+    });
+
 }
 
 const deleteTodo = (todoId) => {
-    throw new Error('Not implemented');
+
+    state.todos = state.todos.filter(todo => todo.id !== todoId); 
+
 }
 
 const deleteCompleted = () => {
-    throw new Error('Not implemented');
+
+    state.todos = state.todos.filter(todo => todo.done);
+
 }
 
+
+/**
+ * 
+ * @param {Filters} newFilter 
+ */
 const setFilter = (newFilter = Filters.All) => {
-    throw new Error('Not implemented');
+
+    state.filter = newFilter;
+
 }
 
 const getCurrentFilter = () => {
-    throw new Error('Not implemented');
+
+    return state.filter;
+
 }
 
 
@@ -63,6 +111,7 @@ export default {
     deleteCompleted,
     deleteTodo,
     getCurrentFilter,
+    getTodos,
     initStore,
     loadStore,
     setFilter,
